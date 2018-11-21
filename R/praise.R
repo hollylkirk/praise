37ca42c540be55f9493510483d7c51add4353868
Holly Kirk <hollylkirk@gmail.com>
2018-11-21 15:13:13 +1100

    this commit will initialise the git repo, first push of the praiseme package

---
+++ R/praise.R
@@ -0,0 +1,12 @@
+#' @title Praise Someone
+#' @description Let's dole out praise
+#' @param name Specifies recipient of praise
+#' @examples praise("Carrot")
+#' @export
+
+praise <- function (name = 'Holly') {
+
+  paste("you are the best, ", name, "!")
+
+
+}
